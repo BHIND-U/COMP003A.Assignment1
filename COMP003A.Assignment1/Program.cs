19f2586286ee0a54fd61f3757f89be1c763fc0e0
joshu <joshu@NZ>
2024-01-21 22:37:21 -0800

    Add project files.

---
+++ COMP003A.Assignment1/Program.cs
@@ -0,0 +1,116 @@
+﻿// Author:  Joshua Isidro
+/* Course:  COMP-003A-L01
+ * Purpose: COde for Assignment 1 
+ */
+
+namespace COMP003A.Assignment1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("100 Things About Me");
+            Console.WriteLine("1. My name is Joshua Isidro");
+            Console.WriteLine("2. I'm 21");
+            Console.WriteLine("3. I am over 70% Native American");
+            Console.WriteLine("4. I’m 5’10”");
+            Console.WriteLine("5. I weigh 165lbs");
+            Console.WriteLine("6. I am fluent in two languages");
+            Console.WriteLine("7. I know some ASL");
+            Console.WriteLine("8. I lived in Mexico for years");
+            Console.WriteLine("9. I have studied in Mexico");
+            Console.WriteLine("10. My favorite food is lasagna");
+            Console.WriteLine("11. I’ve never weighed over 183lbs");
+            Console.WriteLine("12. My goal is to bulk to 190lbs");
+            Console.WriteLine("13. My bench PR is 205lbs");
+            Console.WriteLine("14. I want to bench 3 plates");
+            Console.WriteLine("15. I cannot swim very well");
+            Console.WriteLine("16. I have asthma");
+            Console.WriteLine("17. I would love to visit Japan");
+            Console.WriteLine("18. My favorite candy is sour patch kids");
+            Console.WriteLine("19. My favorite color is midnight blue");
+            Console.WriteLine("20. I want to wrap my car midnight blue");
+            Console.WriteLine("21. My hair is very thick");
+            Console.WriteLine("22. My hair is black");
+            Console.WriteLine("23. My eyes are nearly black");
+            Console.WriteLine("24. My skin tone is light brown");
+            Console.WriteLine("25. I have bushy eyebrows");
+            Console.WriteLine("26. I have a bite scar on my right forearm");
+            Console.WriteLine("27. My hands are somewhat scared from boxing");
+            Console.WriteLine("28. I want to learn Muay Thai");
+            Console.WriteLine("29. My top speed was 130mph");
+            Console.WriteLine("30. I don’t have any tattoos");
+            Console.WriteLine("31. I want to cover up said scar on my forearm");
+            Console.WriteLine("32. I’ve almost died lane splitting");
+            Console.WriteLine("33. I’ve had too many close calls on my motorcycle");
+            Console.WriteLine("34. I took a motorcycle course in Modesto College");
+            Console.WriteLine("35. I’ve never dragged knee");
+            Console.WriteLine("36. I have tinnitus");
+            Console.WriteLine("37. Riding has made my tinnitus worse");
+            Console.WriteLine("38. I want to own an EV");
+            Console.WriteLine("39. I want to change the sprocket on my bike");
+            Console.WriteLine("40. I rarely sleep early");
+            Console.WriteLine("41. My last meal was pasta with chicken");
+            Console.WriteLine("42. My favorite video game is League Of Legends");
+            Console.WriteLine("43. I was so close to diamond last season");
+            Console.WriteLine("44. My favorite champion is Lucian");
+            Console.WriteLine("45. I rarely play other games other than LOL");
+            Console.WriteLine("46. I built my own PC");
+            Console.WriteLine("47. I learned how to build my PC on youtube");
+            Console.WriteLine("48. I listen to all sorts of music");
+            Console.WriteLine("49. I do not listen to rock music");
+            Console.WriteLine("50. My favorite genre would be indie");
+            Console.WriteLine("51. My most listened song last year was Reckless Love");
+            Console.WriteLine("52. I’m a christian");
+            Console.WriteLine("53. I’m planning on getting baptized this summer");
+            Console.WriteLine("54. My vision is not great");
+            Console.WriteLine("55. My favorite CCM song is Oceans");
+            Console.WriteLine("56. I learned ASL in highschool");
+            Console.WriteLine("57. I will become a server next week");
+            Console.WriteLine("58. I work at Harris Ranch");
+            Console.WriteLine("59. I want to become a pilot for fun");
+            Console.WriteLine("60. Learning how vehicles work is interesting to me");
+            Console.WriteLine("61. I want to learn how to pilot a helicopter");
+            Console.WriteLine("62. I’m allergic to dogs");
+            Console.WriteLine("63. I’m allergic to cats");
+            Console.WriteLine("64. I’m also allergic to certain fruits and nuts");
+            Console.WriteLine("65. My favorite fruit are pomegranates");
+            Console.WriteLine("66. I’ve never had long hair");
+            Console.WriteLine("67. My favorite LCS team is 100T");
+            Console.WriteLine("68. I’m in the top 4% in TFT");
+            Console.WriteLine("69. My best mile time is 6:30");
+            Console.WriteLine("70. My favorite cologne is Acqua Di Gio Absolu");
+            Console.WriteLine("71. I want to learn Python");
+            Console.WriteLine("72. I want to learn Java");
+            Console.WriteLine("73. My goal is to become a programmer");
+            Console.WriteLine("74. My favorite movie is Dune");
+            Console.WriteLine("75. The last book I read was Frankeinstein");
+            Console.WriteLine("76. I played my first video game when I was a toddler");
+            Console.WriteLine("77. The worse scar I have is from an iron burn");
+            Console.WriteLine("78. I used to get very sick when I was a kid");
+            Console.WriteLine("79. The last movie I watched was Snake Eyes");
+            Console.WriteLine("80. I did not like watching Snake Eyes");
+            Console.WriteLine("81. I want to get a BS in computer science");
+            Console.WriteLine("82. I took an HTML class in highschool");
+            Console.WriteLine("83. I had a lot of fun making websites");
+            Console.WriteLine("84. 10 keyless is my favorite type of keyboard");
+            Console.WriteLine("85. I also like 40% keyboards");
+            Console.WriteLine("86. I hope the Dolphins win the superbowl");
+            Console.WriteLine("87. I do not like to cook");
+            Console.WriteLine("88. I’ve never voted");
+            Console.WriteLine("89. I’m glad Ron DeSantis stepped out of the race");
+            Console.WriteLine("90. I wonder if Trump will be elected");
+            Console.WriteLine("91. I would like to live in Arizona or Oregon");
+            Console.WriteLine("92. I was in Yuma last week");
+            Console.WriteLine("93. I would like to live in Yuma");
+            Console.WriteLine("94. I’ve only ever had one job");
+            Console.WriteLine("95. Sushi is my favorite Japanese food");
+            Console.WriteLine("96. I would like to go to Vietnam one day");
+            Console.WriteLine("97. I work tomorrow at 7am");
+            Console.WriteLine("98. I rarely work mornings");
+            Console.WriteLine("99. I want to visit the midwest");
+            Console.WriteLine("100. I want to try Chicago");
+        
+        }
+    }
+}
